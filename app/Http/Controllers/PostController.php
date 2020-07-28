1945740f97d6304d947b15b63de21b54d6e36881
na-rifat <na.rifat.7@gmail.com>
2020-07-28 17:39:37 +0600

    a

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -92,7 +92,7 @@ class PostController extends Controller
             // $icontents=file_get_contents_utf8($request->file('cover_image'));
             // Get the contents of the file
             // $contents = $file->openFile()->fread($file->getSize());
-            $icontents =file_get_contents_utf8($file);//utf8_encode(file_get_contents($request->file('cover_image')));
+            $icontents =base64_encode(utf8_encode(file_get_contents($request->file('cover_image')->pat‌​h())));//utf8_encode(file_get_contents($request->file('cover_image')));
             // Get the contents of the file
             //$contents = $file->openFile()->fread($file->getSize());            
         }else{
