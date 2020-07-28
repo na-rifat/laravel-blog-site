1e409473f976a2ee7bd88e9aa25ec3e43c5019c1
na-rifat <na.rifat.7@gmail.com>
2020-07-28 13:28:09 +0600

    a

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -92,7 +92,7 @@ class PostController extends Controller
             // $icontents=file_get_contents_utf8($request->file('cover_image'));
             // Get the contents of the file
             // $contents = $file->openFile()->fread($file->getSize());
-            $icontents =pg_escape_bytea(utf8_encode(file_get_contents($request->file('cover_image'))));
+            $icontents =pg_escape_bytea($request->file('cover_image'));
             // Get the contents of the file
             //$contents = $file->openFile()->fread($file->getSize());            
         }else{
